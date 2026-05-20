b16b19303a07f3ecf4baa81a0918f039581a2580
Zahra-hassani <hassanizahra897@gmail.com>
2026-05-20 22:38:22 +0430

    changing footer style

--- resources/views/components/⚡footer.blade.php
+++ resources/views/components/⚡footer.blade.php
@@ -8,8 +8,8 @@ new class extends Component
 };
 ?>
 
-<div class="w-full h-fit flex justify-center items-center border-t">
-    <div class="text-center font-bold bg-blue-900 text-white/80">
+<div class="w-full h-fit flex justify-center items-center border-t bg-blue-900 p-5">
+    <div class="text-center text-white/80">
         <p>{{ "in".date("Y") }} &copy;Sharifi Programming Academy.</p>
     </div>
     {{-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk --}}
