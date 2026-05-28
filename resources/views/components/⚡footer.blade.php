6b50e20e7834ed4835e30bc66c0fb157c5dd9357
Zahra-hassani <hassanizahra897@gmail.com>
2026-05-28 11:02:17 -0700

    create project and install all requirements

---
+++ resources/views/components/⚡footer.blade.php
@@ -0,0 +1,16 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    //
+};
+?>
+
+<div class="w-full h-fit flex justify-center items-center border-t">
+    <div class="text-center font-bold bg-blue-900 text-white/80">
+        <p>{{ "in".date("Y") }} &copy;Sharifi Programming Academy.</p>
+    </div>
+    {{-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk --}}
+</div>
